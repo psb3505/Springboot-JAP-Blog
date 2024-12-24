ce2ea1ec7df62a38597c7503383cb0c53dedb99c
SeongBeen <kkop109876@gmail.com>
2024-12-24 17:08:18 +0900

    ADD: GET 요청으로 사용자 select 기능 추가

--- src/main/java/com/springboot/myblog/test/DummyControllerTest.java
+++ src/main/java/com/springboot/myblog/test/DummyControllerTest.java
@@ -6,8 +6,13 @@ import com.springboot.myblog.model.RoleType;
 import com.springboot.myblog.model.User;
 import com.springboot.myblog.repository.UserRepository;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 
@@ -20,6 +25,36 @@ public class DummyControllerTest {
     // 이게 의존성 주입(DI)이다.
     @Autowired
     private UserRepository userRepository;
+
+    // {id} 주소로 파라메터를 전달 받을 수 있음
+    // http://localhost:8000/blog/dummy/user/3
+    @GetMapping("/dummy/user/{id}")
+    public User detail(@PathVariable int id) {
+        // user/4를 찾으면 내가 데이터베이스에서 못 찾아오게 되면 user가 null이 될 거다.
+        // 그럼 return null이 리턴이 되기 때문에 프로그램에 문제가 생긴다.
+        // Optional로 User 객체를 감싸 가져와서 null 인지 아닌지 판단해서 return 해야 함
+        User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+            @Override
+            public IllegalArgumentException get() {
+                // TODO Auto-generated method stub
+                return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
+            }
+        });
+
+        // 람다식
+        // User user = userRepository.findById(id).orElseThrow(() -> {
+        //     return new IllegalArgumentException("해당 유저는 없습니다. id : " + id);
+        // });
+
+        // 요청 : 웹브라우저
+        // user 객체 = 자바 오브젝트
+        // 변환 (웹브라우저가 이해할 수 있는 데이터) -> json(Gson 라이브러리)
+        // 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
+        // 만약에 자바 오브젝트를 리턴하게 되면 MessageConverter가 Jacson 라이브러리를 호출해서
+        // user 오브젝트를 json으로 변환해서 브라우저에게 던져준다.
+        return user;
+    }
+    
 
     @PostMapping("/dummy/join")
     public String join(User user) {    // 매개변수에 String username, String password 이런식으로 작성하면 key = value 형식으로 받는다. (약속된 규칙)
